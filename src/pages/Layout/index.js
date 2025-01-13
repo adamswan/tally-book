583879144e243ef458c229ddb4d4d432927bdeb4
adamswan <melyling@sina.com>
2025-01-13 14:12:36 +0800

    开发tabbar完成

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,23 +1,68 @@
-import { Outlet } from "react-router-dom";
-import { Button } from "antd-mobile";
+import { TabBar } from "antd-mobile";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getBillList } from "@/store/modules/billStore";
-import { useEffect } from "react";
+import "./index.scss";
+import {
+  BillOutline,
+  CalculatorOutline,
+  AddCircleOutline,
+} from "antd-mobile-icons";
 
-function Layout() {
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  },
+];
+
+const Layout = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // 用 dispatch 才能调用 store 中的方法
     dispatch(getBillList());
   }, [dispatch]);
 
+  // 切换菜单跳转路由
+  const navigate = useNavigate();
+
+  // 切换面板的回调
+  const handlerChange = (path) => { // 参数path 就是点击的菜单的 key
+    console.log(path);
+    navigate(path);
+  };
+
   return (
-    <div>
-      <Outlet></Outlet>
-      我是layout
-      <Button color="primary">全局生效</Button>
+    <div className="layout">
+      {/* 二级路由的出口 */}
+      <div className="container">
+        <Outlet />
+      </div>
+
+      {/* 底部的 tabbar */}
+      <div className="footer">
+
+        <TabBar onChange={handlerChange}>
+          {tabs.map((item) => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   );
-}
+};
 
 export default Layout;
