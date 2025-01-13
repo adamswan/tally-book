6766d2dc79793e7b5f668c9a6bc3cc563f9f8b53
adamswan <melyling@sina.com>
2025-01-13 13:05:58 +0800

    定制antd的主题色

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { Button } from "antd-mobile";
 
 function Layout() {
   return (
     <div>
       <Outlet></Outlet>
       我是layout
+      <Button color="primary">全局生效</Button>
     </div>
   );
 }
